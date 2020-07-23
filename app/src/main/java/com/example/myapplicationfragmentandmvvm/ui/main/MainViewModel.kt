39ddd5abf70aa388dee56f9ea6cda78d11b83c81
Anton Smyshliaiev <anton.smyshliaiev@headspace.com>
2020-07-23 16:46:00 +0300

    App runs

---
+++ app/src/main/java/com/example/myapplicationfragmentandmvvm/ui/main/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.myapplicationfragmentandmvvm.ui.main
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(
+
+) : ViewModel() {
+
+     init {
+         Log.d("qqq", "MVVM")
+     }
+}
