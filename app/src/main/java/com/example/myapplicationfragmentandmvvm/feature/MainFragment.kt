9022f43d9853395f68598e56c8c33ac9a3d1d116
Anton Smyshliaiev <anton.smyshliaiev@headspace.com>
2020-10-11 13:51:49 +0300

    Fix to get launch

--- app/src/main/java/com/example/myapplicationfragmentandmvvm/feature/MainFragment.kt
+++ app/src/main/java/com/example/myapplicationfragmentandmvvm/feature/MainFragment.kt
@@ -5,8 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.myapplicationfragmentandmvvm.R
+import com.example.myapplicationfragmentandmvvm.feature.data.MyLocalDataSource
+import com.example.myapplicationfragmentandmvvm.feature.data.MyRemoteDataSource
+import com.example.myapplicationfragmentandmvvm.feature.data.MyRepository
+import com.example.myapplicationfragmentandmvvm.feature.data.database.DatabaseFaker
+import com.example.myapplicationfragmentandmvvm.feature.data.network.NetworkFaker
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
@@ -16,7 +22,17 @@ class MainFragment : Fragment() {
             MainFragment()
     }
 
-    private val viewModel: MyViewModel? = null
+    val databaseFaker: DatabaseFaker = DatabaseFaker()
+    val networkFaker: NetworkFaker = NetworkFaker()
+    private val myLocalDataSource: MyLocalDataSource = MyLocalDataSource(databaseFaker)
+    private val myRemoteDataSource: MyRemoteDataSource = MyRemoteDataSource(networkFaker)
+
+    val viewModel: MyViewModel by viewModels {
+        MyViewModelFactory(
+            MyRepository(myLocalDataSource, myRemoteDataSource),
+            MyViewModelState()
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +41,7 @@ class MainFragment : Fragment() {
             message.text = newMessage
         }
 
-        viewModel!!.myViewModelState.textField.observe(this, textFieldObserver)
+        viewModel.myViewModelState.textField.observe(this, textFieldObserver)
     }
 
     override fun onCreateView(
--- app/src/main/java/com/example/myapplicationfragmentandmvvm/feature/MyViewModel.kt
+++ app/src/main/java/com/example/myapplicationfragmentandmvvm/feature/MyViewModel.kt
@@ -2,6 +2,7 @@ package com.example.myapplicationfragmentandmvvm.feature
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.myapplicationfragmentandmvvm.feature.data.MyRepository
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
@@ -23,4 +24,18 @@ class MyViewModel constructor(
             }
     }
 
+}
+
+class MyViewModelFactory(myRepository: MyRepository, myViewModelState: MyViewModelState) :
+    ViewModelProvider.Factory {
+    val myRepository: MyRepository
+    val myViewModelState: MyViewModelState
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MyViewModel(myRepository, myViewModelState) as T
+    }
+
+    init {
+        this.myRepository = myRepository
+        this.myViewModelState = myViewModelState
+    }
 }
